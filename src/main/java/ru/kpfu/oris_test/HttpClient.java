b1a9a7a9870ab87c408da320fae1340d589d85cf
ktsaur <taryshkinakv@gmail.com>
2025-01-18 14:28:24 +0300

    test18_01

--- src/main/java/ru/kpfu/oris_test/HttpClient.java
+++ src/main/java/ru/kpfu/oris_test/HttpClient.java
@@ -3,7 +3,7 @@ package ru.kpfu.oris_test;
 import java.io.IOException;
 import java.util.Map;
 
-public interface HttpClient {
+public interface HttpClient{
     String get(String url, Map<String, String> headers, Map<String, String> params) throws IOException;
     String post(String url, Map<String, String> headers, Map<String, String> data) throws IOException;
     String put(String url, Map<String, String> headers, Map<String, String> data) throws IOException;
